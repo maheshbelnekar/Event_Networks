86f85270836299c701ee604ed1117ae51218afc3
mbelnek <mbelnek@iu.edu>
2018-12-19 15:49:52 -0500

    Linked login and register activities

--- app/src/main/java/com/projects/maheshbelnekar/event_networks/LoginActivity.java
+++ app/src/main/java/com/projects/maheshbelnekar/event_networks/LoginActivity.java
@@ -1,13 +1,45 @@
 package com.projects.maheshbelnekar.event_networks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private EditText userEmail, userPassword;
+    private TextView needNewAccountLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initializing all my elements
+        needNewAccountLink = (TextView)findViewById(R.id.register_account_link);
+        userEmail = (EditText)findViewById(R.id.login_email);
+        userPassword = (EditText)findViewById(R.id.login_password);
+        loginButton = (Button)findViewById(R.id.login_button);
+
+        // set a listener for signup link
+        needNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Send him to register
+                sendUserToRegisterActivity();
+            }
+        });
+
+    }
+
+    private Boolean sendUserToRegisterActivity() {
+
+        Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(registerIntent);
+        return true;
     }
 }
