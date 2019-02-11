1e6f83628f6d03b8c67e86f40a9982b7b99968f7
Mahesh Belnekar <mbelnek@iu.edu>
2019-02-10 21:25:45 -0500

    Added support actionbar to post activity

--- app/src/main/java/com/projects/maheshbelnekar/event_networks/PostActivity.java
+++ app/src/main/java/com/projects/maheshbelnekar/event_networks/PostActivity.java
@@ -1,13 +1,45 @@
 package com.projects.maheshbelnekar.event_networks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class PostActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mToolbar = findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setTitle("Update Post");
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == android.R.id.home){
+            sendUserToMainActivity();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Boolean sendUserToMainActivity() {
+
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        mainIntent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        return true;
     }
 }
