1f06ba9c6dedb9142c6b3b822700ea89b41fb190
Mahesh Belnekar <mbelnek@iu.edu>
2019-02-11 23:54:34 -0500

    Initialized buttons on post activity

--- app/src/main/java/com/projects/maheshbelnekar/event_networks/PostActivity.java
+++ app/src/main/java/com/projects/maheshbelnekar/event_networks/PostActivity.java
@@ -5,21 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ImageButton selectPostImage;
+    private Button updatePostButton;
+    private EditText postDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        selectPostImage = findViewById(R.id.select_post_image);
+        updatePostButton = findViewById(R.id.update_post_button);
+        postDescription = findViewById(R.id.post_description);
         mToolbar = findViewById(R.id.update_post_page_toolbar);
         setSupportActionBar(mToolbar);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setTitle("Update Post");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
     }
 
 
